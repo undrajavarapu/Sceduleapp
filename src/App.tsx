5faa2f606f1c1fc0dc1198b31378a9c477effcef
undrajavarapu <undrajavarapu@gmail.com>
2024-03-17 04:22:17 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
+import Schedules from "./components/Schedules";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"></div>
+      <div className="container-fluid">
+        <div className="row"></div>
+        <div className="row">
+          <div className="col-auto col-sm-1" id="sideNav"></div>
+          <div className="col-auto col-sm-11" id="topNav"></div>
+        </div>
+      </div>
+      <div className="container">
+        <Schedules></Schedules>
+      </div>
     </div>
   );
 }
